efc1e7ce7578e856d2dd2619bbbe41e0585b4af9
liman <1310742648@qq.com>
2025-03-01 17:05:52 +0800

    修改页面切换方式

--- src/popup/components/create/index.vue
+++ src/popup/components/create/index.vue
@@ -7,8 +7,11 @@ export default {
 </script>
 <script lang="ts" setup>
 import bus from '@/utils/bus.js';
+import { useRouter } from 'vue-router';
+let router = useRouter();
 const toCreate = (res: any) => {
-  bus.emit('nextPage', res);
+  // bus.emit('nextPage', res);
+  router.push('/'+res);
 };
 </script>
 <style lang="scss">
