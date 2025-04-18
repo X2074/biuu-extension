6e90dbfe8f89cd7d05d69ee1eee7d4bb6ad1939f
liman <1310742648@qq.com>
2025-04-18 16:13:13 +0800

    代码回退

--- src/popup/app.vue
+++ src/popup/app.vue
@@ -44,9 +44,6 @@ import { getBlance } from '@/utils/index';
 import indexDbData from '@/utils/indexDB.js';
 import bus from '@/utils/bus.js';
 import { useRouter } from 'vue-router';
-import { useSyncProviders } from '@/store/useSyncProviders.ts';
-const providers = useSyncProviders()
-console.log(providers, 'providers')
 let loading = ref(true);
 let loadingText = ref('加载中...');
 const userAddress = ref(null);
